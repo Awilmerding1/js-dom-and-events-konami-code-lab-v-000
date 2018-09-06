dcffd165e97185229d801eb61d896e2a95707f0a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-06 17:21:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  document.body.addEventListener('keydown', (function(e) {
+  document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
       index++;
@@ -13,7 +13,7 @@ function init() {
     } else {
       index = 0;
     }
-  })
+  }
 }
 
 
